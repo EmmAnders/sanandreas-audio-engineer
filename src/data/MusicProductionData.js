8791b45dfaf4bce133fb01b61c266a1f1c66be3c
Emma <emmandersson166@gmail.com>
2021-06-05 16:59:08 +0200

    -

---
+++ src/data/MusicProductionData.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const MusicProductionData = [
+  {
+    id: uuidv4(),
+    title: "San Andreas - Double U",
+    soundCloudEmbed:
+      "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/833899528&color=%23262626&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+  },
+];
