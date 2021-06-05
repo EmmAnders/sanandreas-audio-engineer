a04df57065519b8b0f02d6384916c455a66ba585
Emma <emmandersson166@gmail.com>
2021-06-05 18:07:03 +0200

    render data from json

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import { MusicProductionData } from "../data/MusicProductionData";
-import { TechProjectsData } from "../data/MusicProductionData";
+import { TechProjectsData } from "../data/TechProjectsData";
 import { Tabs, Tab } from "../components/Tabs";
 import "../scss/pages/Projects.scss";
 
@@ -8,14 +8,44 @@ const Projects = () => {
     <main className="main">
       <Tabs>
         <Tab label={"All"} tabName={"ALL"}>
-          <p>All</p>
+          <div>
+            {TechProjectsData.map((data, i) => {
+              return (
+                <div key={data.id}>
+                  <h2>{data.title}</h2>
+                  <p>{data.descShort}</p>
+                </div>
+              );
+            })}
+
+            {MusicProductionData.map((data, i) => {
+              return (
+                <div key={data.id}>
+                  <p>{data.title}</p>
+                </div>
+              );
+            })}
+          </div>
         </Tab>
         <Tab label={"Tech Projects"} tabName={"TECH PROJECTS"}>
-          <p>TECH PROJECTS</p>
+          {TechProjectsData.map((data, i) => {
+            return (
+              <div key={data.id}>
+                <h2>{data.title}</h2>
+                <p>{data.descShort}</p>
+              </div>
+            );
+          })}
         </Tab>
 
         <Tab label={"Music Production"} tabName={"MUSIC PRODUCTION"}>
-          <p>MUSIC PRODUCTION</p>
+          {MusicProductionData.map((data, i) => {
+            return (
+              <div key={data.id}>
+                <p>{data.title}</p>
+              </div>
+            );
+          })}
         </Tab>
       </Tabs>
     </main>
